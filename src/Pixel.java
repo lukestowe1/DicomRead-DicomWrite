41cd37b48db1ea0aa00baedf10e4a858e7863a1b
Luke Stowe <lukestowe@Lukes-MacBook.local>
2015-01-11 01:07:25 +0000

    Latest Pixel data access

---
+++ src/Pixel.java
@@ -0,0 +1,17 @@
+/**
+ * Created by lukestowe on 01/12/14.
+ */
+public class Pixel {
+    private Byte first;
+    private Byte second;
+
+    Pixel(Byte b1,Byte b2)
+    {
+        first=b1;
+        second=b2;
+    }
+
+
+
+
+}
--- src/Reader.java
+++ src/Reader.java
@@ -1,39 +1,51 @@
+/*
+Author Luke Stowe
+Tag Along Ashley Robert Deane
+ */
+
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+
 
 class Reader {
     public static void main(String[] argv) throws Exception {
         //Read from an input stream
         InputStream is = new BufferedInputStream(new FileInputStream("im0.dcm"));
         DataInputStream in = new DataInputStream(is);
-        byte [] bytes = new byte[20001];
-
+        Byte [] bytes = new Byte[600000];
+        Byte [] endOfhead1 = {(byte)0xe0,(byte)0x7f,(byte)0x10,(byte)0x00,(byte)0x4f,
+                (byte)0x57,(byte)0x00,(byte)0x00,(byte)0x00,(byte)0x00,(byte)0x08,(byte)0x00};//512
+        //Byte [] endOfhead2 = {(byte)0xe0,(byte)0x7f,(byte)0x10,(byte)0x00,(byte)0x4f,
+        // (byte)0x57,(byte)0x00,(byte)0x00,(byte)0x80,(byte)0x40,(byte)0x08,(byte)0x00};//520
 
 
         //handle writing back to a file
         BufferedOutputStream o =new BufferedOutputStream(new FileOutputStream("im2.dcm"));
         DataOutputStream out = new DataOutputStream(o);
-
+        int count = 0;
         try
         {
-            while(in.available()> 0x0)// end of file
+            while(in.available() > 0)// end of file + count size
             {
-                bytes [i] = in.readByte();
+                bytes [count] = in.readByte();
+                count++;
             }
-            System.out.println((char)bytes[128]);
+            int pixelStart=Collections.indexOfSubList(Arrays.asList(bytes),Arrays.asList(endOfhead1))+endOfhead1.length;
+            System.out.println("Pixel values start at : "+pixelStart);
 
-
-
+            short firstPix = (short) ((bytes[3241] << 8) & 0x3F | (bytes[3240]  ));
+            System.out.println("First pixel: "+ firstPix);
             //test to change byte value from D to I
             bytes[128]=73;
-            System.out.println((char)bytes[128]);
+
             for(int i=0;i<2001;i++)
             {
                 //write to new  image
                 out.writeByte(bytes[i]);
-
-
             }
+
 
             // close stuff
             out.close();
