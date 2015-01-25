15da731c958bdda0a0fa68a4c37736d4a60f399c
Clodagh Upton <clodaghupton@Clodaghs-MacBook-Air.local>
2015-01-25 18:37:47 +0000

    2d array for pixels

--- src/Reader.java
+++ src/Reader.java
@@ -20,6 +20,8 @@ class Reader {
         Byte [] endOfhead2 = {(byte)0xe0,(byte)0x7f,(byte)0x10,(byte)0x00,(byte)0x4f,
          (byte)0x57,(byte)0x00,(byte)0x00,(byte)0x80,(byte)0x40,(byte)0x08,(byte)0x00};//520
 
+        int pSize = 520;
+        int [][]pixelData = new int[pSize][pSize];
 
         //handle writing back to a file
         BufferedOutputStream o =new BufferedOutputStream(new FileOutputStream("im2.dcm"));
@@ -36,13 +38,22 @@ class Reader {
             int pixelStart=Collections.indexOfSubList(Arrays.asList(bytes),Arrays.asList(endOfhead2))+endOfhead2.length;
             System.out.println("Pixel values start at : "+pixelStart);
 
-            int firstPix = (bytes[12829] )& 0xFFFF; //& 0x0FFF | (bytes[293548]  ))& 0xFFFF;
-             firstPix = firstPix << 8;
-            int e = ( bytes[1828])& 0xff ;
-            firstPix = firstPix | e;
-            System.out.println("First pixel: "+ firstPix);
-            System.out.println();
 
+            for(int i = 0; i < pSize ; i++)
+            {
+                for(int j = 0; j < pSize*2;j+=2) { //increment by two so dont merge wrong bytes
+                    int firstPix = (bytes[pixelStart+1]) & 0xFFFF; //& 0x0FFF | (bytes[293548]  ))& 0xFFFF;
+                    firstPix = firstPix << 8;
+                    int e = (bytes[pixelStart]) & 0xff;
+                    firstPix = firstPix | e;
+                    pixelData[i][j/2]=firstPix;
+                    pixelStart+=2;
+                    System.out.println(j/2);
+                }
+                System.out.println(i);
+            }
+
+            System.out.println(pixelData[250][300]);
 
             //test to change byte value from D to I
             bytes[128]=73;
