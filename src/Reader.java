0b07fbdfd864635e9392f8e0c87f582c7ed793fb
Clodagh Upton <luke.stowe2@gmail.com>
2015-01-27 20:33:42 +0000

    Addition of comments and refactoring of code

--- src/Reader.java
+++ src/Reader.java
@@ -12,21 +12,21 @@ import java.util.Collections;
 class Reader {
     public static void main(String[] argv) throws Exception {
         //Read from an input stream
-        InputStream is = new BufferedInputStream(new FileInputStream("Phantom_Artifact.dcm"));
+        InputStream is = new BufferedInputStream(new FileInputStream("Phantom_Artifact.dcm"));//input image
         DataInputStream in = new DataInputStream(is);
-        Byte [] bytes = new Byte[600000];
-        Byte [] endOfhead1 = {(byte)0xe0,(byte)0x7f,(byte)0x10,(byte)0x00,(byte)0x4f,
-                (byte)0x57,(byte)0x00,(byte)0x00,(byte)0x00,(byte)0x00,(byte)0x08,(byte)0x00};//512
-        Byte [] endOfhead2 = {(byte)0xe0,(byte)0x7f,(byte)0x10,(byte)0x00,(byte)0x4f,
-         (byte)0x57,(byte)0x00,(byte)0x00,(byte)0x80,(byte)0x40,(byte)0x08,(byte)0x00};//520
+        Byte[] bytes = new Byte[600000];//array to store all bytes of file
+        Byte[] endOfhead1 = {(byte) 0xe0, (byte) 0x7f, (byte) 0x10, (byte) 0x00, (byte) 0x4f,
+                (byte) 0x57, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x08, (byte) 0x00};//End of head array for 512.dcm file
+        Byte[] endOfhead2 = {(byte) 0xe0, (byte) 0x7f, (byte) 0x10, (byte) 0x00, (byte) 0x4f,
+                (byte) 0x57, (byte) 0x00, (byte) 0x00, (byte) 0x80, (byte) 0x40, (byte) 0x08, (byte) 0x00};//End of head array for 520.dcm file
 
-        int pSize = 520;
-        int positionToWrite=0;
-        int pixelStart=0;
+        int pSize = 520;//size of pixel array either 512/520 will get set further down
+        int positionToWrite = 0;//position in bytes to start writing all the new pixels out to
+        int pixelStart = 0;//start position of pixels in terms of bytes
 
 
         //handle writing back to a file
-        BufferedOutputStream o =new BufferedOutputStream(new FileOutputStream("Output.dcm"));
+        BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream("Output.dcm"));
         DataOutputStream out = new DataOutputStream(o);
         int count = 0;
         try {
@@ -43,54 +43,48 @@ class Reader {
                 pSize = 512;
             }
 
-            Pixel[][] pixelData = new Pixel[pSize][pSize];
+            Pixel[][] pixelData = new Pixel[pSize][pSize];//take in pixel objects
             positionToWrite = pixelStart;  //position to write back to
             System.out.println("Pixel values start at : " + pixelStart);
 
-            int [][]metalHalves = new int [100][2];
-            int count2 =0;
-            int arrayCount=0;
-            int flag=1;
+            int[][] metalHalves = new int[100][2];//store middle values of any metal object detected in the image
+            int count2 = 0;
+            int arrayCount = 0;
+            int flag = 1;
             //int[][] metal = new int[pSize][pSize];
             for (int i = 0; i < pSize; i++) {
                 for (int j = 0; j < pSize * 2; j += 2) { //increment by two so dont merge wrong bytes
-                    Pixel p = new Pixel(bytes[pixelStart + 1], bytes[pixelStart]);
+                    Pixel p = new Pixel(bytes[pixelStart + 1], bytes[pixelStart]);//change every byte to Pixel
                     pixelData[i][j / 2] = p;
                     pixelStart += 2;
-                    if (p.getPixelValue() > 4000) {
+                    if (p.getPixelValue() > 4000) {//detect for metal object
                         //metal[i][j/2]=1;
                         count2++;
-                        flag=0;
-                    }
-                    else
-                    {
-                        if(flag==0)
+                        flag = 0;
+                    } else {
+                        if (flag == 0)//after metal has been detected calculate horizontal middle value
                         {
-                            metalHalves[arrayCount][1]=(j/2)-(count2/2);
-                            metalHalves[arrayCount][0]=i;
-                            count2=0;
+                            metalHalves[arrayCount][1] = (j / 2) - (count2 / 2);
+                            metalHalves[arrayCount][0] = i;
+                            count2 = 0;
                             arrayCount++;
-                            flag=1;
+                            flag = 1;//reset the flag
                         }
                         //metal[i][j/2]=0;
 
                     }
                 }
             }
-            for(int i = 0; i<30;i++)
+            for (int i = 0; i < 30; i++)//test print out middle values
             {
-                for(int j = 0 ; j<1;j++)
-                {
-                    System.out.println(metalHalves[i][j]+" "+metalHalves[i][j+1]);
+                for (int j = 0; j < 1; j++) {
+                    System.out.println(metalHalves[i][j] + " " + metalHalves[i][j + 1]);
 
                 }
             }
 
 
-
-
-            for(int i=0;i<50000;i++)
-            {
+            for (int i = 0; i < 50000; i++) {
                 //write to new  image
                 out.writeByte(bytes[i]);
             }
@@ -98,13 +92,9 @@ class Reader {
 
             // close stuff
             out.close();
-        }
-
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
 
 
     }
